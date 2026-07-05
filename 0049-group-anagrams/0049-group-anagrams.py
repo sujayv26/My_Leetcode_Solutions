838edda59af8305f7c888817abf22a74dd287265
sujayv26 <Sujayv2126@gmail.com>
2026-07-05 15:12:33 +0530

    Time: 15 ms (96.3%), Space: 16 MB (84.68%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        res = defaultdict(list)
+
+        for s in strs:
+
+            sortedS = ''.join(sorted(s))
+
+            res[sortedS].append(s)
+
+        return list(res.values())  
+        
