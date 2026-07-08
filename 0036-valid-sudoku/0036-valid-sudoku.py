bf32280e430531580030a7a0150a08d1df561f17
sujayv26 <Sujayv2126@gmail.com>
2026-07-08 13:24:44 +0530

    Time: 4 ms (78.5%), Space: 12.3 MB (58.9%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,30 +1,20 @@
 class Solution(object):
     def isValidSudoku(self, board):
-        for row in range(9):
-            seen = set()
-            for i in range(9):
-                if board[row][i] == ".":
-                   continue
-                if board[row][i] in seen:
+        cols = defaultdict(set)
+        rows = defaultdict(set)
+        squares = defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if ( board[r][c] in rows[r]
+                    or board[r][c] in cols[c]
+                    or board[r][c] in squares[(r // 3, c // 3)]):
                     return False
-                seen.add(board[row][i])
-        for col in range(9):
-            seen = set()
-            for i in range(9):
-                if board[i][col] == ".":
-                   continue
-                if board[i][col] in seen:
-                    return False
-                seen.add(board[i][col])
-        for square in range(9):
-            seen = set()
-            for i in range(3):
-                for j in range(3):
-                    row = (square // 3)*3 + i
-                    col = (square % 3)*3 + j
-                    if board[row][col] == ".":
-                        continue 
-                    if board[row][col] in seen:
-                        return False
-                    seen.add(board[row][col])
+
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
+
         return True
